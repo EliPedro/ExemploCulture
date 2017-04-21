b5ce63fdfbcdf38208f29eef7898b798d21ec1ff
eli <elipedrodos.santos@gmail.com>
2017-04-21 14:05:41 -0300

    OK

---
+++ ResourcesGlobalization/ResourcesGlobalization.Mvc/Controllers/GlobalizationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Globalization;
+
+namespace ResourcesGlobalization.Mvc.Controllers
+{
+    public class GlobalizationController : Controller
+    {
+        // GET: Globalization
+        [HttpGet]
+        public ActionResult Index()
+        {
+            decimal valor = Convert.ToDecimal(4530.56);
+            DateTime data = new DateTime(2017, 4, 21);
+
+            var ciBr = new CultureInfo("pt-br");
+
+            ViewBag.Valor = valor.ToString("c",ciBr);
+            ViewBag.Data = data.ToString("D",ciBr);
+
+            var ci = new CultureInfo("en-us");
+
+            ViewBag.ValorUs = valor.ToString("c", ci);
+            ViewBag.DataUs = data.ToString("D", ci);
+
+
+            return View();
+        }
+    }
+}
---
+++ ResourcesGlobalization/ResourcesGlobalization.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ResourcesGlobalization.Mvc.MvcApplication" Language="C#" %>
